2eec82a893f4ef8464192a297c6726b55bbd9777
gayan5214214 <gayan@codelantic.com>
2024-08-06 14:55:47 +0530

    canges

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-DEFAULT_USER = User.objects.get(id=1) 
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
